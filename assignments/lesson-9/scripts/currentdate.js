734276e2a7a5ba5525a5cc8223090dedf4f097c0
drtrumpet1 <drtrumpet1@yahoo.com>
2019-06-20 21:50:28 -0500

    Lesson 9 JSON Update

---
+++ assignments/lesson-9/scripts/currentdate.js
@@ -0,0 +1,24 @@
+var today = new Date();
+var dd = today.getDate();
+var ww = today.getDay();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+
+if(dd<10) {
+    dd = '0'+ dd
+} 
+
+if(mm<10) {
+    mm = '0'+ mm
+} 
+
+document.getElementById("date").innerHTML=today = weekday[ww] + ", " + dd + mm + yyyy;
